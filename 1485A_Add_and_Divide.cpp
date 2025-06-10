41e72271447b79d9c544458d733beeffe6de9758
syedamaheenkazmi172 <maheenkazmi172@gmail.com>
2025-06-09 23:27:52 -0700

    added comments

--- 1485A_Add_and_Divide.cpp
+++ 1485A_Add_and_Divide.cpp
@@ -11,7 +11,7 @@ int main()
         cin >> a >> b;
         int c = 1;
 
-        if (b >= 6)
+        if (b >= 6) //after 6 we do not try to add into b because it starts to give larger values
         {
             int x = a;
             int count = 0;
@@ -27,22 +27,20 @@ int main()
             int min_count = INT_MAX;
             for (int i = b; i <= 6; i++)
             {
-                int count = i - b;
+                int count = i - b; //checks how many times we have to make b to reach i-b 
                 int x = a;
-                if (i >= 2)
+                if (i >= 2) //if i is equal to 1, it is of no use to keep on dividing a by it
                 {
                     while (x > 0)
                     {
                         x /= i;
                         count++;
                     }
-
                     min_count = min(count, min_count);
                 }
             }
             cout << min_count << endl;
         }
     }
-
     return 0;
 }
