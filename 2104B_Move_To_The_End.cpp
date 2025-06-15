674917d3334afbd221108b50584880ef5c2055c7
syedamaheenkazmi172 <maheenkazmi172@gmail.com>
2025-06-15 00:35:06 -0700

    edit

--- 2104B_Move_To_The_End.cpp
+++ 2104B_Move_To_The_End.cpp
@@ -17,9 +17,7 @@ int main()
         {
             cin >> a[i];
         }
-
-        vector<long long> b(k);
-
+         
         vector<long long> pre_max(k);
         vector<long long> pre_sum(k);
         pre_max[k - 1] = a[0];
@@ -31,7 +29,7 @@ int main()
         pre_sum[0]=0;
         for (int i = 1; i < k; i++)
         {
-            pre_sum[i] = pre_sum[i - 1] + a[k - i];
+            pre_sum[i] = pre_sum[i - 1] + a[k - i]; 
         }
         
         for (int i = 0; i < k; i++)
